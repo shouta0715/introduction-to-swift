b3ff0fd9424b3a87330fa548e166e80b97b47c40
shouta0715 <kshouta0715@gmail.com>
2025-01-26 00:19:22 +0900

    function

--- Contents.swift
+++ Contents.swift
@@ -1,1 +1,25 @@
+func muFunction(number:Int) -> Int {
+    return number * 2
+}
 
+var myNumber = 5
+
+myNumber = muFunction(number: myNumber)
+
+
+
+func doubleFuction(number:Int) -> Int {
+  number * 2
+}
+
+myNumber = 5
+
+myNumber = doubleFuction(number: myNumber)
+
+
+func second(value: inout Int) {
+  value = value * 2
+}
+myNumber = 10
+
+second(value: &myNumber)
