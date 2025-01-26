0d9aab61353b76f04de7d2e64a7406cf31a1f761
shouta0715 <kshouta0715@gmail.com>
2025-01-26 21:04:48 +0900

    strcut basic

--- Contents.swift
+++ Contents.swift
@@ -11,3 +11,30 @@ struct Item {
     name = newName
   }
 }
+
+struct Price {
+  var USD: Double
+  var CAD: Double
+  
+  var canadians: Double {
+    get {USD * 1.25}
+    set {USD = newValue / 1.25}
+  }
+  
+  init(americans: Double) {
+    USD = americans
+    CAD = americans * 1.25
+  }
+  
+  init(canadians: Double) {
+    CAD = canadians
+    USD = canadians / 1.25
+  }
+}
+
+var price: Price = .init(canadians: 100)
+var item: Item = .init(name: "Shoes", price: price.USD)
+
+
+var totalPrice = item.totalPrice(quantity: 2)
+
