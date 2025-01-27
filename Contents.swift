b9aad2a2f343feda78f7cdf3af4fbf5ad07d9483
shouta0715 <kshouta0715@gmail.com>
2025-01-27 20:18:46 +0900

    set

--- Contents.swift
+++ Contents.swift
@@ -1,5 +1,35 @@
-struct MyStructure<T> {
-  var myvalue: T
+var list: [Int] = [1, 2, 3, 4, 5]
+
+
+for (index, value) in list.enumerated() {
+  let message = "Item \(index + 1): \(value)"
+  
+  print(message)
 }
 
-let i = MyStructure<Int>(myvalue: 42)
+list.removeAll { Int in
+  return Int % 2 == 0
+}
+
+let half = list.map { $0 / 2 }
+
+var list3:Array<Array<Int>> = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+let toatl = list.reduce(0, +)
+
+var ages:Set<Int> = [1, 2, 3, 4, 1]
+
+let names:Set<String> = ["A", "B", "C", "D"]
+
+for age in ages {
+  let message = "Age: \(age)"
+  ages.insert(5)
+  let removed = ages.remove(5)
+  
+  print(message)
+}
+
+let isContains = names.contains("A")
+
+let sorted = names.sorted()
+
