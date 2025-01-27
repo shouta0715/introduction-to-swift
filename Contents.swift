0b2b524777355e8d42d242c3c31d1cc9f598c7a2
shouta0715 <kshouta0715@gmail.com>
2025-01-27 22:06:39 +0900

    class

--- Contents.swift
+++ Contents.swift
@@ -1,76 +1,15 @@
 
-enum Number {
-  case one, two, three
-  
-  func getMessage() -> String {
-    switch self {
-    case .one:
-      return "one message"
-    case .two:
-      return "two message"
-    case .three:
-      return "three message"
-    }
-  }
+class Employee {
+  var name = "Unknown"
+  var age = 0
 }
 
-let one = Number.one
-let two = Number.two
-let three = Number.three
+let employee = Employee()
+let employee2 = employee
 
-let mynumber = Number.one
+employee.name
 
-switch mynumber {
-  case .one:
-    print("one")
-  case .two:
-    print("two")
-  case .three:
-    print("three")
-  
-  
-}
+employee.name = "John"
+employee.age = 30
 
-
-enum Number2: String {
-  case one = "one"
-  case two = "two"
-  case three = "three"
-}
-
-let mynumber2 = Number2.one
-let rawValue = mynumber2.rawValue
-
-mynumber.getMessage()
-
-enum MyCharacters {
-  case number(Int, String)
-  case letter(Character, String)
-  
-  init?(rawValue: Int) {
-    switch rawValue {
-    case ...9:
-      self = .number(rawValue, "number")
-    default:
-      self = .letter("A", "letter")
-    }
-  }
-}
-
-var char = MyCharacters.number(1, "one")
-
-let res:String = switch char {
-  case .number(1, "one"):
-    "number"
-  case .number(let value, let description):
-    "number: \(value) - \(description)"
-  case .letter(let letter, let description):
-    "letter: \(letter) - \(description)"
-}
-
-if case .number(1, "one") = char {
-  let message = "number"
-    
-}
-
-let n = Number2(rawValue:   "one")
+employee2.name
