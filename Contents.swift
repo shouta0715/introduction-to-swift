fb5ad53cf935d3b7d5975f8c233712ece1688ad6
shouta0715 <kshouta0715@gmail.com>
2025-01-29 22:13:09 +0900

    as is

--- Contents.swift
+++ Contents.swift
@@ -1,15 +1,40 @@
-
 class Employee {
-  var name = "Unknown"
+  var name: String = "Unknown"
   var age = 0
 }
 
-let employee = Employee()
-let employee2 = employee
+class OfficeEmployee: Employee {
+  var deskNumber = 0
+}
 
-employee.name
+class WarehouseEmployee: Employee {
+  var area = "Unknown"
 
-employee.name = "John"
-employee.age = 30
+}
 
-employee2.name
+var list: [Employee] = [OfficeEmployee(), WarehouseEmployee(),OfficeEmployee()]
+
+//for employee in list {
+//  if employee is OfficeEmployee {
+//    print("Office Employee \(employee.age)")
+//  } else if employee is WarehouseEmployee {
+//    let warehouseEmployee = employee as! WarehouseEmployee
+//    print("Warehouse Employee \(warehouseEmployee.area)")
+//  } else {
+//    print("Employee \(employee.age)")
+//  }
+//}
+//
+
+for employee in list {
+  if let officeEmployee = employee as? OfficeEmployee {
+    let message = "Office Employee \(officeEmployee.deskNumber)"
+  } else if let warehouseEmployee = employee as? WarehouseEmployee {
+    let message = "Warehouse Employee \(warehouseEmployee.area)"
+  } else {
+    let message = "Employee \(employee.name)"
+  }
+}
+
+
+
