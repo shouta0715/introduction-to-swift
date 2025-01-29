1c20d5701f7c4130abf9e4957cf751468bce1420
shouta0715 <kshouta0715@gmail.com>
2025-01-29 22:28:06 +0900

    init

--- Contents.swift
+++ Contents.swift
@@ -1,40 +1,20 @@
 class Employee {
-  var name: String = "Unknown"
-  var age = 0
-}
-
-class OfficeEmployee: Employee {
-  var deskNumber = 0
-}
-
-class WarehouseEmployee: Employee {
-  var area = "Unknown"
-
-}
-
-var list: [Employee] = [OfficeEmployee(), WarehouseEmployee(),OfficeEmployee()]
-
-//for employee in list {
-//  if employee is OfficeEmployee {
-//    print("Office Employee \(employee.age)")
-//  } else if employee is WarehouseEmployee {
-//    let warehouseEmployee = employee as! WarehouseEmployee
-//    print("Warehouse Employee \(warehouseEmployee.area)")
-//  } else {
-//    print("Employee \(employee.age)")
-//  }
-//}
-//
-
-for employee in list {
-  if let officeEmployee = employee as? OfficeEmployee {
-    let message = "Office Employee \(officeEmployee.deskNumber)"
-  } else if let warehouseEmployee = employee as? WarehouseEmployee {
-    let message = "Warehouse Employee \(warehouseEmployee.area)"
-  } else {
-    let message = "Employee \(employee.name)"
+  var name: String
+  var age: Int
+  
+  init(name: String, age: Int) {
+    self.name = name
+    self.age = age
+  }
+  
+  convenience init(name: String) {
+    self.init(name: name, age: 0)
+  }
+  
+  convenience init() {
+    self.init(name: "un",age: 0)
   }
 }
 
-
-
+let employee = Employee(name: "John", age: 30)
+let employee2 = Employee()
