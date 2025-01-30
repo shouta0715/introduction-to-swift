8ba2b2c8bdb4afbd2c3e20d15a3966466f86d363
shouta0715 <kshouta0715@gmail.com>
2025-01-30 22:22:05 +0900

    error do cache and result

--- Contents.swift
+++ Contents.swift
@@ -7,12 +7,11 @@ enum Errors: Error {
 struct Stock {
   var totalLamps = 5
   
-  mutating func sold(amount: Int) throws {
-    if totalLamps < amount {
-        throw Errors.outOfStock
-    } else {
-      totalLamps -= amount
+  mutating func sold(amount: Int) -> Result<Int, Errors> {
+    guard totalLamps >= amount else {
+      return .failure(Errors.outOfStock)
     }
+    return .success(Int(totalLamps - amount))
   }
 }
   
@@ -20,13 +19,24 @@ struct Stock {
 
 var stock = Stock()
 
-do {
-  try stock.sold(amount: 10)
-} catch Errors.outOfStock {
-  let error = Errors.outOfStock
-  let message = "Sorry, we only have \(stock.totalLamps) lamps left."
-  
-  print("\(error): \(message)")
-}
+let result = stock.sold(amount: 3)
 
-try? stock.sold(amount: 10)
+
+//switch result {
+//case .success(let stock):
+//  print("Stock left: \(stock)")
+//case .failure(let error):
+//  if error == .outOfStock {
+//    print("Sorry, we are out of stock")
+//  } else {
+//    print("An unknown error occured")
+//  }
+//}
+
+do {
+  let stock = try result.get()
+  print("Stock left: \(stock)")
+} catch Errors.outOfStock {
+//  そのままエラーを投げる
+  throw Errors.outOfStock  
+}
