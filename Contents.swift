1a645f5d3c41148d2f9c79685f9dc5e38e020d60
shouta0715 <kshouta0715@gmail.com>
2025-01-31 22:44:54 +0900

    CoreGraphics

--- Contents.swift
+++ Contents.swift
@@ -1,35 +1,8 @@
 import Foundation
+import CoreGraphics
 
-
-let message = "Name: John Doe"
-
-let regex = /Name:\s+[a-zA-Z]+ [a-zA-Z]+/
-
-if let match = message.firstMatch(of: regex) {
-  let found = match.output
-  
-  print("Found: \(found)")
-}
-
-
-
-let matches = message.matches(of: regex)
-
-if !matches.isEmpty {
-  let names = matches.map { $0.output }
-  
-  let list = names.joined(separator: ", ")
-  print("Found: \(list)")
-}
-
-let m = """
-Apple
-Banana
-Cherry
-Date
-"""
-
-let separator = /\n/
-
-let results = m.replacing(separator, with: ", ")
+let float: CGFloat = .zero
+let point: CGPoint = .zero
+let size: CGSize = .zero
+let rect: CGRect = .zero
 
