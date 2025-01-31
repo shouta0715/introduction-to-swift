d0387c58d2f29dde489325c39a88e302153f4864
shouta0715 <kshouta0715@gmail.com>
2025-01-31 21:59:21 +0900

    Measurement

--- Contents.swift
+++ Contents.swift
@@ -1,64 +1,11 @@
 import Foundation
 
-let currentDate = Date()
-let nextDate = currentDate.addingTimeInterval(60*60*24)
-let nextDate2 = Date(timeIntervalSinceNow: 60*60*24)
 
-if currentDate.compare(nextDate) == .orderedAscending {
-  let message = "nextDate is in the future"
-  
-  print(message)
-}
+var length = Measurement(value: 100, unit: UnitLength.meters)
+var width = Measurement(value: 5, unit: UnitLength.kilometers)
 
-let calendar = Calendar.current
-var components = calendar.dateComponents(
-  [.year, .month, .day],
-  from: currentDate
-)
+let total = (length + width).converted(to: .kilometers)
 
-var components2 = DateComponents()
+length.converted(to: .kilometers)
 
-components2.year = 2020
-components2.month = 1
-components2.day = 1
-
-let date = "\(components.year!)-\(components.month!)-\(components.day!)"
-
-var birthday = calendar.date(from: components2)
-
-
-
-let id  = Calendar.Identifier.gregorian
-let calendar2 = Calendar(identifier: id)
-var components3 = DateComponents()
-
-components3.day = 120
-
-var today = Date()
-var appointment = calendar2.date(byAdding: components3, to: today)
-
-var components4 = DateComponents()
-
-components4.year = 2002
-components4.month = 7
-components4.day = 3
-
-let birthday2 = calendar.date(from: components4)
-
-if let olddate = birthday2 {
-  let components = calendar.dateComponents([.day], from: olddate,to: today)
-  
-  let message = "It has been \(components.day!) days since my birthday"
-  
-  print(message)
-}
-
-
-let mydate = Date.now
-
-let formatted = mydate.formatted(.dateTime.weekday(.wide))
-
-let formatted2 = mydate.formatted(.dateTime.day().hour().minute().second())
-
-let jsLocal = Locale(identifier: "ja_JP")
-let text = mydate.formatted(.dateTime.locale(jsLocal).day().month().year())
+length.formatted(.measurement(width: .wide, usage: .person))
